fc6da5cde5e6e0fe6f75d05ab5a217ae4d085335
fernandomonterrosa124-cmyk <fernandomonterrosa124@gmail.com>
2026-05-02 23:12:36 -0600

    Refactor InventarioDAL to implement IInventarioDAL

--- SY_PROSERA/PROSERA.Datos/InventarioDAL.cs
+++ SY_PROSERA/PROSERA.Datos/InventarioDAL.cs
@@ -4,7 +4,41 @@ using System.Text;
 
 namespace PROSERA.Datos
 {
-    internal class InventarioDAL
+      public class InventarioDAL : IInventarioDAL
     {
+        public DataTable Listar()
+        {
+            using SqlConnection cn = new(ConexionDB.Cadena);
+            cn.Open();
+
+            string sql = @"SELECT i.id_inventario, p.nombre, i.stock, i.stock_minimo
+                           FROM Inventario i
+                           INNER JOIN Productos p 
+                           ON i.id_producto = p.id_producto";
+
+            using SqlDataAdapter da = new(sql, cn);
+            DataTable dt = new();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public void ActualizarStock(int idProducto, int cantidad, string tipo)
+        {
+            using SqlConnection cn = new(ConexionDB.Cadena);
+            cn.Open();
+
+            string operacion = tipo == "Entrada" ? "+" : "-";
+
+            string sql = $@"UPDATE Inventario
+                            SET stock = stock {operacion} @cantidad,
+                                fecha_actualizacion = GETDATE()
+                            WHERE id_producto = @idProducto";
+
+            using SqlCommand cmd = new(sql, cn);
+            cmd.Parameters.Add("@cantidad", SqlDbType.Int).Value = cantidad;
+            cmd.Parameters.Add("@idProducto", SqlDbType.Int).Value = idProducto;
+
+            cmd.ExecuteNonQuery();
+        }
     }
 }
